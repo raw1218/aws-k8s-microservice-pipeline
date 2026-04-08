1772d1c9236fee9d6fd1db2546fe2ffa55c57119
raw1218 <raw1218@gmail.com>
2026-04-08 14:09:22 -0400

    Added Staging Overlay

--- eks/create-eks-vpc.sh
+++ eks/create-eks-vpc.sh
@@ -12,3 +12,17 @@ aws iam create-role \
 
 aws eks update-kubeconfig --region us-east-1 --name EKS-Dev-Cluster
             
+
+aws iam create-role \
+  --role-name myAmazonEKSNodeRole \
+  --assume-role-policy-document file://"node-role-trust-policy.json"
+
+aws iam attach-role-policy \
+  --policy-arn arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy \
+  --role-name myAmazonEKSNodeRole
+aws iam attach-role-policy \
+  --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly \
+  --role-name myAmazonEKSNodeRole
+aws iam attach-role-policy \
+  --policy-arn arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy \
+  --role-name myAmazonEKSNodeRole
